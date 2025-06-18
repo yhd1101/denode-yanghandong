79546d53a8805172e5048e82edb501991a35f33e
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2025-06-19 00:56:12 +0900

    feat: 응답 통일 및 예외 처리용 인터셉터, 필터 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BaseAPIDocument } from './common/config/swagger.document';
 import { SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +17,10 @@ async function bootstrap() {
     credentials: true
   })
 
-  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); //적용을하겠다
 
   const config = new BaseAPIDocument().initializeOptions();
   const document = SwaggerModule.createDocument(app, config);
