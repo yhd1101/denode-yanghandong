23de63923e7a66d31a39ac5c1525ba131d068476
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2025-06-20 02:23:37 +0900

    feat: rename take → size

--- src/common/dtos/page-options.dto.ts
+++ src/common/dtos/page-options.dto.ts
@@ -30,9 +30,9 @@ export class PageOptionsDto {
   @Min(1)
   @Max(50)
   @IsOptional()
-  readonly take?: number = 8;
+  readonly size?: number = 8;
 
   get skip(): number {
-    return (this.page - 1) * this.take;
+    return (this.page - 1) * this.size;
   }
 }
